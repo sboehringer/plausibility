3804afa145546a4039c8fd0cb553ebfdd48c5aaa
Stefan Böhringer <github@s-boehringer.org>
2021-06-29 18:05:29 +0200

    Commit for onward development of version 0.4-1

--- R/plausibilityWeighted.R
+++ R/plausibilityWeighted.R
@@ -8,9 +8,11 @@ require('glmnet');
 #	<p> weighted models
 #
 
+log1pExp = function(x, scale = 1) ifelse (x * scale < -5, x, log(log1p(exp(x * scale))) / scale) 
+
 cumProbSIcomp = function(par, this) {
 	N = length(this@y);
-	Nsim = ncol(this@sim);
+	Nsim = this@Nsi;
 	lp = (this@mm %*% par)[, 1];
 	# help optimizer, if necessary (e.g. bound away form 0, 1), defaults to identity
 	lp = plausBounder(this@model, lp);
@@ -19,23 +21,25 @@ cumProbSIcomp = function(par, this) {
 	# importance sampling, cancel out uniform factor (max)
 	TsIS0 = TsRaw - this@pSI;
 	#TsIS = TsIS0 - max(TsIS0);
-	TsIS = TsIS0;
+	#TsIS = TsIS0;
+	#Ts = log1pExp( TsIS0 );	# dampen weighing
+	Ts = TsIS0;
 
 	#<A> comparison based on ll instead of -ll, keep on thies scale for IS correction
-	sel = this@weight > this@weights;       # this is constant <N>
-	Ts = TsIS[sel];
+	#sel = this@weight > this@weights;       # this is constant <N>
+	#Ts = TsIS[sel];
 	#Nsim = sumExp(TsIS);    #Nsim = ncol(this@sim);
 	#if (length(Nsim) == 0) return(1e50 * .Machine$double.xmin);
 
 	# <p> P-value
-	eps = .1 / ncol(this@sim);
+	eps = .1 / Nsim;
 	#eps = 0;
 	P = if (any(Ts == -Inf) || length(Ts) == 0)
 			(eps / 2) / (Nsim + eps) else
 			((sumExp(Ts) + eps) / (Nsim + eps));
 	#if (P > 1) browser();
 	#dprint(par, P);
-	return(P);
+	return(1 - P);
 }
 
 #
@@ -46,16 +50,19 @@ weightingFunctionLR = function(model, y, mm0, mm1) {
 	m0 = plausFit(model, y, mm0);
 	m1 = plausFit(model, y, mm1);
 	# ordering in favor of alternative has to be small values first
-	return(-2*( m1$ll - m0$ll));
+	#return(-2*( m1$ll - m0$ll ));
+	# <!> 28.6.2021 changed to integrate the tail to achieve more stable P-values,
+	#	dispense with factor 2
+	return( m1$ll - m0$ll );
 }
 
 setClass("plausibilityGlmWeighted", contains = 'plausibilityFamilyWeightedSI', representation = list(
 	pSI = 'numeric',
 	model = 'plausibilityModel',
 	mdl0 = 'list',		# glm model + corresponing ll
-	sim = 'matrix',
-	weights = 'numeric',	# simulation weights
-	weight = 'numeric'		# data weight
+	sim = 'matrix'
+	#weights = 'numeric',	# simulation weights
+	#weight = 'numeric'		# data weight
 ), prototype = list());
 
 setMethod('initialize', 'plausibilityGlmWeighted', function(.Object, f0, f1, data, Nsi = 1e3, model,
@@ -84,14 +91,17 @@ setMethod('initialize', 'plausibilityGlmWeighted', function(.Object, f0, f1, dat
 	if (is.null(sim))
 		sim = apply(matrix(runif(length(lp) * Nsi), ncol = Nsi), 2, function(u)
 			plausSample(model, u, lp, parAncil));
-	.Object@sim = sim;
+
+	# <p> simulation weights
+	weights = apply(sim, 2, function(r)weightingFunction(model, r, .Object@mm, mm1));
+	# <p> data weights
+	weight = weightingFunction(model, .Object@y, .Object@mm, mm1);
+
+	# filter simulation for required sub-sample
+	.Object@sim = sim[, weights <= weight ];
 
 	# <p> probabilities stochastic integration sample under starting pars, needed for importance correction
-	.Object@pSI = apply(sim, 2, function(r)plausDensity(model, r, lp, parAncil));
-	# <p> simulation weights
-	.Object@weights = apply(sim, 2, function(r)weightingFunction(model, r, .Object@mm, mm1));
-	# <p> data weights
-	.Object@weight = weightingFunction(model, .Object@y, .Object@mm, mm1);
+	.Object@pSI = apply(.Object@sim, 2, function(r)plausDensity(model, r, lp, parAncil));
 
 	return(.Object);
 });
